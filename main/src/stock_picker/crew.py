afb0150df6ac02b72f7e25a9d98ad85d874edca1
Hooopla <Johngabrielbello@outlook.com>
2025-11-09 17:49:15 -0800

    progress :3:

---
+++ main/src/stock_picker/crew.py
@@ -0,0 +1,66 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from typing import List
+from crewai.tools import SerperDevTool
+
+@CrewBase
+class StockPicker():
+    """StockPicker crew"""
+
+    agents: List[BaseAgent]
+    tasks: List[Task]
+
+    @agent
+    def trending_company_finder(self) -> Agent:
+        return Agent(
+            config=self.agents_config['trending_company_finder'], # type: ignore[index]
+            verbose=True
+        )
+
+    @agent
+    def financial_researcher(self) -> Agent:
+        return Agent(
+            config=self.agents_config['financial_researcher'], # type: ignore[index]
+            verbose=True
+        )
+
+    @agent
+    def stock_picker(self) -> Agent:
+        return Agent(
+            config=self.agents_config['stock_picker'], # type: ignore[index]
+            verbose=True
+        )
+
+    @task
+    def find_trending_companies(self) -> Task:
+        return Task(
+            config=self.tasks_config['find_trending_companies'], # type: ignore[index]
+            output_file='output/trending_companies.json'
+        )
+
+    @task
+    def research_trending_companies(self) -> Task:
+        return Task(
+            config=self.tasks_config['research_trending_companies'], # type: ignore[index]
+            output_file='output/research_companies.json'
+        )
+    
+    @task
+    def pick_best_company(self) -> Task:
+        return Task(
+            config=self.tasks_config['pick_best_company'], # type: ignore[index]
+            output_file='output/decision.json'
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the StockPicker crew"""
+
+        return Crew(
+            agents=self.agents, # Automatically created by the @agent decorator
+            tasks=self.tasks, # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True,
+            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+        )
